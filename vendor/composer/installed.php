b94f3fad7787d30d95b59f012ae43f31100b67a6
Morsel <misha5486@mail.ru>
2024-09-27 20:18:34 +0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'morsel/minesweeper',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a741b2d38788c77a01286f6765828bdfb588abfb',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'morsel/minesweeper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a741b2d38788c77a01286f6765828bdfb588abfb',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.0',
+            'version' => '0.12.0.0',
+            'reference' => '2d1da4751f47622b56e6616c87b2fb53decee4e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
